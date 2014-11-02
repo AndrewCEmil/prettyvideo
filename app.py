3cc36a7443d9e5d0ee5cab50f3e6acc59eaa6fac
AndrewCEmil <andrew.emil@10gen.com>
2014-11-02 01:30:37 -0400

    Added template file

--- app.py
+++ app.py
@@ -11,4 +11,12 @@ def index(name):
 def canvas(filename):
     return static_file(filename, root='./static/')
 
+def get_shader_str():
+    return 'gl_FragColor = vec4(abs(sin(vTime.x * 3.14159)), 0.0, 0.0, 1.0);'
+
+@route('/shade')
+def shade():
+    shaderstr = get_shader_str()
+    return template('./templates/copytimetest.html', shaderstr=shaderstr)
+
 run(host='localhost', port=8080)
